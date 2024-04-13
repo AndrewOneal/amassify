d5af69accc2900fc75ad3dcfd037e72801997ec7
Andrew Oneal <112568256+AndrewOneal@users.noreply.github.com>
2024-04-13 01:20:07 -0400

    auth working

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,31 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const {data: session} = useSession();
+  const [x, setX] = useState('');
+  const [profile, setProfile] = useState([]);
+  useEffect(() => {
+    async function f() {
+      if (session && session.accessToken) {
+        setX(session.accessToken)
+        const response = await fetch("https://api.spotify.com/v1/me", {
+          headers: {
+            Authorization: `Bearer ${session.accessToken}`
+          }
+        })
+        const data = await response.json();
+        setProfile(data.display_name);
+        console.log('data');
+      }
+    }
+    f();
+  },[session]);
+
   return (
     <main>
-     
+      <div>access token: {x}</div>
+      <div>Username: {profile}</div>
     </main>
   );
 }
---
+++ pages/login.jsx
@@ -0,0 +1,11 @@
+import { signIn } from 'next-auth/react';
+
+const Login = () => {
+    return (
+        <div className='flex items-center justify-center'>
+            <button onClick={()=>signIn('spotify', { callbackUrl: "/"}) }>Login with Spotify</button>
+        </div>
+    );
+}
+
+export default Login;
