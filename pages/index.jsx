842a49b519f014692aeaee371f570b020cde39f8
Andrew Oneal <112568256+AndrewOneal@users.noreply.github.com>
2024-04-14 01:09:26 -0400

    progress on profile

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,22 @@
-import { useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
-import { signOut } from 'next-auth/react';
-import BottomNav from '../components/bottom_nav.jsx';
-import Image from 'next/image';
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { signOut } from "next-auth/react";
+import BottomNav from "../components/bottom_nav.jsx";
+import Image from "next/image";
 
 export default function Home() {
-  const {data: session} = useSession();
+  const { data: session } = useSession();
   const [profileData, setProfileData] = useState([]);
   const [topTracks, setTopTracks] = useState([]);
+  const [topArtists, setTopArtists] = useState([]);
   useEffect(() => {
     async function getProfileData() {
       if (session && session.accessToken) {
         const response = await fetch("https://api.spotify.com/v1/me", {
           headers: {
-            Authorization: `Bearer ${session.accessToken}`
-          }
-        })
+            Authorization: `Bearer ${session.accessToken}`,
+          },
+        });
         const data = await response.json();
         setProfileData(data);
       }
@@ -23,64 +24,119 @@ export default function Home() {
 
     async function getTopTracks() {
       if (session && session.accessToken) {
-        const response = await fetch("https://api.spotify.com/v1/me/top/tracks?time_range=short_term", {
-          headers: {
-            Authorization: `Bearer ${session.accessToken}`
+        const response = await fetch(
+          "https://api.spotify.com/v1/me/top/tracks?time_range=short_term",
+          {
+            headers: {
+              Authorization: `Bearer ${session.accessToken}`,
+            },
           }
-        })
+        );
         const data = await response.json();
         setTopTracks(data);
+      }
+    }
+
+    async function getTopArtists() {
+      if (session && session.accessToken) {
+        const response = await fetch(
+          "https://api.spotify.com/v1/me/top/artists?time_range=short_term",
+          {
+            headers: {
+              Authorization: `Bearer ${session.accessToken}`,
+            },
+          }
+        );
+        const data = await response.json();
+        setTopArtists(data);
         console.log(data);
       }
     }
 
     getProfileData();
     getTopTracks();
-  },[session]);
+    getTopArtists();
+  }, [session]);
   return (
-    <main className='pt-20 pl-20'>
-      <div className='flex flex-col items-start'>
+    <main className="pt-10 pl-20">
+      <div class="text-white text-8xl pb-20 font-bold">Amassify</div>
+      <div className="flex flex-col items-center text-center">
         <div className="avatar">
-            <div className="w-24 rounded-xl">
-              <img src={profileData.images && profileData.images.length > 1 ? profileData.images[1].url : 'default_image_url'} alt="user image" />
-            </div>
-            <p class='text-white px-8 py-2 font-bold text-lg text-5xl'>Welcome to your profile, {profileData.display_name}!</p>
+          <div className="w-24 rounded-xl">
+            <img
+              src={
+                profileData.images && profileData.images.length > 1
+                  ? profileData.images[1].url
+                  : "default_image_url"
+              }
+              alt="user image"
+            />
+          </div>
+          <p class="text-white px-8 py-2 font-bold text-5xl">
+            {profileData.display_name}
+          </p>
         </div>
         <div>
-        <p class='text-white pt-10 py-2 font-bold text-lg text-5xl'> Top played </p>
-        <div className="collapse collapse-arrow bg-base-200">
-          <input type="radio" name="my-accordion-2" defaultChecked /> 
-          <div className="collapse-title text-xl font-medium">
-            Click to open this one and close others
-          </div>
-          <div className="collapse-content"> 
-            <p>hello</p>
-          </div>
+          <p class="text-white pt-10 pb-10 py-2 font-bold text-4xl">
+            Top played
+          </p>
         </div>
-        <div className="collapse collapse-arrow bg-base-200">
-          <input type="radio" name="my-accordion-2" /> 
-          <div className="collapse-title text-xl font-medium">
-            Click to open this one and close others
-          </div>
-          <div className="collapse-content"> 
-            <p>hello</p>
-          </div>
+        <div className="max-h-[20rem] overflow-y-auto">
+          <table className="table">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Track</th>
+                <th>Artist</th>
+                <th>Album</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {topTracks.items &&
+                topTracks.items.map((track, index) => (
+                  <tr key={index}>
+                    <td>
+                      <div className="font-bold">#{index + 1}</div>
+                    </td>
+                    <td>
+                      <div className="flex items-center gap-3">
+                        <div className="avatar">
+                          <div className="w-12 h-12">
+                            <img
+                              src={
+                                track.album
+                                  ? track.album.images[0].url
+                                  : "default_image_url"
+                              }
+                              alt="album_img"
+                            />
+                          </div>
+                        </div>
+                        <div className="font-bold">{track.name}</div>
+                      </div>
+                    </td>
+                    <th>
+                      <div className="font-bold">{track.artists[0].name}</div>
+                    </th>
+                    <th>
+                      <div className="font-bold">{track.album.name}</div>
+                    </th>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
         </div>
-        <div className="collapse collapse-arrow bg-base-200">
-          <input type="radio" name="my-accordion-2" /> 
-          <div className="collapse-title text-xl font-medium">
-            Click to open this one and close others
-          </div>
-          <div className="collapse-content"> 
-            <p>hello</p>
-          </div>
-        </div>
-        </div>
-        <div class='pt-10 pb-20'>
-            <button className='text-white px-8 py-2 rounded-full bg-green-500 font-bold text-lg align' onClick={()=>signOut('spotify', { callbackUrl: "/login"}) }>Logout</button>
+        <div class="pt-10 pb-20">
+          <button
+            className="text-white px-8 py-2 rounded-full bg-green-500 font-bold text-lg align"
+            onClick={() => signOut("spotify", { callbackUrl: "/login" })}
+          >
+            Logout
+          </button>
         </div>
       </div>
-      <BottomNav class='pt-20' />
+      <BottomNav class="pt-20" />
     </main>
   );
 }
