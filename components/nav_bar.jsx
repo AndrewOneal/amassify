e509aba8d2f645b2336bae70382e6d50d11cfa75
Andrew Oneal <112568256+AndrewOneal@users.noreply.github.com>
2024-04-16 21:51:33 -0400

    fixed logo and created profile route, might not keep it

--- components/nav_bar.jsx
+++ components/nav_bar.jsx
@@ -34,58 +34,66 @@ function NavBar() {
   return (
     <header class="header top-0 bg-gray-900 shadow-md flex items-center justify-between px-8 py-02 z-1">
       <h1 class="w-3/12">
-        <a href="">
-          <div className="flex items-center">
-            <svg
-              width="160px"
-              height="60px"
-              viewBox="0 0 48 48"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
+        <div
+          className="flex items-center cursor-pointer"
+          onClick={() => {
+            setSelectedOption((prev) => {
+              const newValue = "Profile";
+              localStorage.setItem("selectedOption", newValue);
+              return newValue;
+            });
+            router.push("/");
+          }}
+        >
+          <svg
+            width="160px"
+            height="60px"
+            viewBox="0 0 48 48"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              {" "}
+              <rect width="48" height="48" fill="gray-900"></rect>{" "}
+              <path
+                d="M42 30V24.4615C42 14.2655 33.9411 6 24 6C14.0589 6 6 14.2655 6 24.4615V30"
+                stroke="#000000"
+                stroke-width="4"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                {" "}
-                <rect width="48" height="48" fill="gray-900"></rect>{" "}
-                <path
-                  d="M42 30V24.4615C42 14.2655 33.9411 6 24 6C14.0589 6 6 14.2655 6 24.4615V30"
-                  stroke="#000000"
-                  stroke-width="4"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></path>{" "}
-                <path
-                  d="M34 32C34 29.7909 35.7909 28 38 28H42V42H38C35.7909 42 34 40.2091 34 38V32Z"
-                  fill="#1DB954"
-                  stroke="#000000"
-                  stroke-width="4"
-                  stroke-linejoin="round"
-                ></path>{" "}
-                <path
-                  d="M42 32H44C45.1046 32 46 32.8954 46 34V36C46 37.1046 45.1046 38 44 38H42V32Z"
-                  fill="#000000"
-                ></path>{" "}
-                <path
-                  d="M6 32H4C2.89543 32 2 32.8954 2 34V36C2 37.1046 2.89543 38 4 38H6V32Z"
-                  fill="#000000"
-                ></path>{" "}
-                <path
-                  d="M6 28H10C12.2091 28 14 29.7909 14 32V38C14 40.2091 12.2091 42 10 42H6V28Z"
-                  fill="#1DB954"
-                  stroke="#000000"
-                  stroke-width="4"
-                  stroke-linejoin="round"
-                ></path>{" "}
-              </g>
-            </svg>
-            <p className="text-white py-3 font-bold text-4xl ml-0">Amassify</p>
-          </div>
-        </a>
+              ></path>{" "}
+              <path
+                d="M34 32C34 29.7909 35.7909 28 38 28H42V42H38C35.7909 42 34 40.2091 34 38V32Z"
+                fill="#1DB954"
+                stroke="#000000"
+                stroke-width="4"
+                stroke-linejoin="round"
+              ></path>{" "}
+              <path
+                d="M42 32H44C45.1046 32 46 32.8954 46 34V36C46 37.1046 45.1046 38 44 38H42V32Z"
+                fill="#000000"
+              ></path>{" "}
+              <path
+                d="M6 32H4C2.89543 32 2 32.8954 2 34V36C2 37.1046 2.89543 38 4 38H6V32Z"
+                fill="#000000"
+              ></path>{" "}
+              <path
+                d="M6 28H10C12.2091 28 14 29.7909 14 32V38C14 40.2091 12.2091 42 10 42H6V28Z"
+                fill="#1DB954"
+                stroke="#000000"
+                stroke-width="4"
+                stroke-linejoin="round"
+              ></path>{" "}
+            </g>
+          </svg>
+          <p className="text-white py-3 font-bold text-4xl ml-0">Amassify</p>
+        </div>
       </h1>
       <nav class="nav font-semibold text-lg">
         <ul class="flex items-center">
--- pages/insights.jsx
+++ pages/insights.jsx
@@ -1,7 +1,6 @@
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
-import { signOut } from "next-auth/react";
-import BottomNav from "../components/nav_bar.jsx";
+import NavBar from "../components/nav_bar.jsx";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -22,30 +21,7 @@ export default function Home() {
   }, [session]);
   return (
     <main>
-      <style jsx>{`
-        img {
-          border-radius: 50%;
-        }
-        text {
-          font-size: 140px;
-        }
-      `}</style>
-      <img
-        src={
-          profileData.images && profileData.images.length > 1
-            ? profileData.images[1].url
-            : "default_image_url"
-        }
-        alt="user image"
-      />
-      <p>Username: {profileData.display_name}</p>
-      <button
-        className="text-white px-8 py-2 rounded-full bg-green-500 font-bold text-lg"
-        onClick={() => signOut("spotify", { callbackUrl: "/login" })}
-      >
-        Logout
-      </button>
-      <BottomNav />
+      <NavBar />
     </main>
   );
 }
