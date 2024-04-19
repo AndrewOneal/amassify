44545a636172c6a3cc4254c4d3afc496ea8da29d
Andrew Oneal <112568256+AndrewOneal@users.noreply.github.com>
2024-04-19 01:29:36 -0400

    progress on ratings

--- pages/index.jsx
+++ pages/index.jsx
@@ -25,6 +25,7 @@ export default function Home() {
 
   const getTopTracks = useCallback(async () => {
     if (session && session.accessToken) {
+      console.log("called");
       const response = await fetch(
         `https://api.spotify.com/v1/me/top/tracks?time_range=${timeframe}`,
         {
--- pages/ratings.jsx
+++ pages/ratings.jsx
@@ -1,7 +1,8 @@
 import NavBar from "@/components/nav_bar";
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { DndContext } from "@dnd-kit/core";
+import PocketBase from "pocketbase";
 
 // get data from database on load
 // query spotify with array of song, album, artist ids
@@ -12,9 +13,55 @@ import { DndContext } from "@dnd-kit/core";
 // delete button on each item, delete item from database, refresh list of items
 // delete everything button, delete all items from database, refresh list of items??
 
-export default function Home() {
+const pb = new PocketBase("https://amassify.pockethost.io");
+
+export default function Ratings() {
   const { data: session } = useSession();
-  useEffect(() => {}, [session]);
+  const [profileData, setProfileData] = useState([]);
+
+  const checkForExistingRankings = useCallback(async (id) => {
+    if (id) {
+      try {
+        const record = await pb
+          .collection("ratings")
+          .getFirstListItem(`spotify_user_ID=${id}`);
+        console.log(record);
+      } catch (error) {
+        console.log(error);
+        console.log(id);
+        try {
+          const record = await pb.collection("ratings").create({
+            spotify_user_ID: id,
+            rankings: [],
+          });
+          console.log(record);
+        } catch (error) {}
+      }
+    }
+  }, []);
+
+  const getProfileData = useCallback(
+    async (accessToken, id) => {
+      if (accessToken) {
+        const response = await fetch("https://api.spotify.com/v1/me", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        const data = await response.json();
+        setProfileData(data);
+        checkForExistingRankings(id);
+      }
+    },
+    [checkForExistingRankings]
+  );
+
+  useEffect(() => {
+    if (session && session.accessToken) {
+      getProfileData(session.accessToken, session.user.id);
+    }
+  }, [session, getProfileData]);
+
   return (
     <main>
       <NavBar />
