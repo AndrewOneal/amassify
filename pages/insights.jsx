c06faa2bb16f268bc828d9368c8500ec2a42c40e
Andrew Oneal <112568256+AndrewOneal@users.noreply.github.com>
2024-04-22 10:08:41 -0400

    insights almost done

--- pages/insights.jsx
+++ pages/insights.jsx
@@ -3,15 +3,12 @@ import { useEffect, useState } from "react";
 import NavBar from "../components/nav_bar.jsx";
 import { useCallback } from "react";
 
-// get top artists
-// get top tracks
-// get recently played
-
 export default function Insights() {
   const { data: session } = useSession();
   const [profileData, setProfileData] = useState([]);
   const [topTracks, setTopTracks] = useState([]);
   const [topArtists, setTopArtists] = useState([]);
+  const [recentlyPlayed, setRecentlyPlayed] = useState([]);
   const [timeframe, setTimeframe] = useState("short_term");
 
   const getProfileData = useCallback(async () => {
@@ -56,31 +53,225 @@ export default function Insights() {
     }
   }, [session, timeframe]);
 
+  const getRecentlyPlayed = useCallback(async () => {
+    if (session && session.accessToken) {
+      const response = await fetch(
+        `https://api.spotify.com/v1/me/player/recently-played`,
+        {
+          headers: {
+            Authorization: `Bearer ${session.accessToken}`,
+          },
+        }
+      );
+      const data = await response.json();
+      setRecentlyPlayed(data);
+    }
+  }, [session]);
+
+  function getTrackAveragePopularity() {
+    let sum = 0;
+    topTracks.items.forEach((track) => {
+      sum += track.popularity;
+    });
+    return sum / topTracks.items.length;
+  }
+
+  function getArtistAveragePopularity() {
+    let sum = 0;
+    console.log(topArtists.items);
+    topArtists.items.forEach((artist) => {
+      sum += artist.popularity;
+    });
+    console.log(sum / topArtists.items.length);
+    return sum / topArtists.items.length;
+  }
+
+  function getTopArtistGenres() {
+    let genres = {};
+    topArtists.items.forEach((artist) => {
+      artist.genres.forEach((genre) => {
+        if (genres[genre]) {
+          genres[genre] += 1;
+        } else {
+          genres[genre] = 1;
+        }
+      });
+    });
+    let sortedGenres = Object.keys(genres).sort(
+      (a, b) => genres[b] - genres[a]
+    );
+    return sortedGenres.slice(0, 5);
+  }
+
+  async function getTopTrackGenres() {
+    if (session && session.accessToken) {
+      const artistIds = [
+        ...new Set(topTracks.items.map((item) => item.artists[0].id)),
+      ].join(",");
+
+      const response = await fetch(
+        `https://api.spotify.com/v1/artists/?ids=${artistIds}`,
+        {
+          headers: {
+            Authorization: `Bearer ${session.accessToken}`,
+          },
+        }
+      );
+      const artistData = await response.json();
+
+      let genres = {};
+      artistData.artists.forEach((artist) => {
+        artist.genres.forEach((genre) => {
+          if (genres[genre]) {
+            genres[genre] += 1;
+          } else {
+            genres[genre] = 1;
+          }
+        });
+      });
+      let sortedGenres = Object.keys(genres).sort(
+        (a, b) => genres[b] - genres[a]
+      );
+      return sortedGenres.slice(0, 5);
+    }
+  }
+
+  async function getRecentTrackGenres() {
+    if (session && session.accessToken) {
+      const artistIds = [
+        ...new Set(
+          recentlyPlayed.items.map((item) => item.track.artists[0].id)
+        ),
+      ].join(",");
+
+      const response = await fetch(
+        `https://api.spotify.com/v1/artists/?ids=${artistIds}`,
+        {
+          headers: {
+            Authorization: `Bearer ${session.accessToken}`,
+          },
+        }
+      );
+      const artistData = await response.json();
+
+      let genres = {};
+      artistData.artists.forEach((artist) => {
+        artist.genres.forEach((genre) => {
+          if (genres[genre]) {
+            genres[genre] += 1;
+          } else {
+            genres[genre] = 1;
+          }
+        });
+      });
+      let sortedGenres = Object.keys(genres).sort(
+        (a, b) => genres[b] - genres[a]
+      );
+      console.log(sortedGenres.slice(0, 5));
+      return sortedGenres.slice(0, 5);
+    }
+  }
+
+  async function getTopAudioFeatures() {
+    if (session && session.accessToken) {
+      const trackIds = topTracks.items.map((item) => item.id).join(",");
+      const response = await fetch(
+        `https://api.spotify.com/v1/audio-features/?ids=${trackIds}`,
+        {
+          headers: {
+            Authorization: `Bearer ${session.accessToken}`,
+          },
+        }
+      );
+      const trackFeatureData = await response.json();
+
+      let features = {
+        danceability: 0,
+        energy: 0,
+        speechiness: 0,
+        acousticness: 0,
+        instrumentalness: 0,
+        liveness: 0,
+        valence: 0,
+      };
+      trackFeatureData.audio_features.forEach((audioFeature) => {
+        features.danceability += audioFeature.danceability;
+        features.energy += audioFeature.energy;
+        features.speechiness += audioFeature.speechiness;
+        features.acousticness += audioFeature.acousticness;
+        features.instrumentalness += audioFeature.instrumentalness;
+        features.liveness += audioFeature.liveness;
+        features.valence += audioFeature.valence;
+      });
+      for (const feature in features) {
+        features[feature] = features[feature] / topTracks.items.length;
+      }
+      let sortedFeatures = Object.keys(features).sort(
+        (a, b) => features[b] - features[a]
+      );
+      return sortedFeatures.slice(0, 3);
+    }
+  }
+
+  async function getRecentAudioFeatures() {
+    if (session && session.accessToken) {
+      const trackIds = recentlyPlayed.items
+        .map((item) => item.track.id)
+        .join(",");
+      const response = await fetch(
+        `https://api.spotify.com/v1/audio-features/?ids=${trackIds}`,
+        {
+          headers: {
+            Authorization: `Bearer ${session.accessToken}`,
+          },
+        }
+      );
+      const trackFeatureData = await response.json();
+
+      let features = {
+        danceability: 0,
+        energy: 0,
+        speechiness: 0,
+        acousticness: 0,
+        instrumentalness: 0,
+        liveness: 0,
+        valence: 0,
+      };
+      trackFeatureData.audio_features.forEach((audioFeature) => {
+        features.danceability += audioFeature.danceability;
+        features.energy += audioFeature.energy;
+        features.speechiness += audioFeature.speechiness;
+        features.acousticness += audioFeature.acousticness;
+        features.instrumentalness += audioFeature.instrumentalness;
+        features.liveness += audioFeature.liveness;
+        features.valence += audioFeature.valence;
+      });
+      for (const feature in features) {
+        features[feature] = features[feature] / recentlyPlayed.items.length;
+      }
+      let sortedFeatures = Object.keys(features).sort(
+        (a, b) => features[b] - features[a]
+      );
+      console.log(sortedFeatures.slice(0, 3));
+      return sortedFeatures.slice(0, 3);
+    }
+  }
+
   useEffect(() => {
     getProfileData();
     getTopTracks();
     getTopArtists();
-  }, [getProfileData, getTopTracks, getTopArtists]);
+    getRecentlyPlayed();
+  }, [getProfileData, getTopTracks, getTopArtists, getRecentlyPlayed]);
+
   return (
     <main>
       <NavBar className="z-1" />
       <div className="pt-5 pl-20 pr-20">
         <div className="flex flex-col">
-          <div className="avatar flex items-center m-4">
-            <div className="w-24 rounded-xl z-0">
-              <img
-                src={
-                  profileData.images && profileData.images.length > 1
-                    ? profileData.images[1].url
-                    : "https://upload.wikimedia.org/wikipedia/commons/b/b5/Windows_10_Default_Profile_Picture.svg"
-                }
-                alt="user image"
-              />
-            </div>
-            <p className="text-white px-8 py-2 font-bold text-5xl">
-              {profileData.display_name}
-            </p>
-          </div>
+          <p className="text-white py-2 font-bold text-5xl">
+            Welcome to your insights, {profileData.display_name}
+          </p>
           <div className="timeframe-selector pt-10">
             <select
               value={timeframe}
@@ -94,108 +285,42 @@ export default function Insights() {
               <option value="long_term">Lifetime</option>
             </select>
           </div>
-          <div className="table-container">
-            <p className="text-white pt-10 pb-10 py-2 font-bold text-4xl">
-              Top Played Tracks
-            </p>
-            <div className="max-h-[20rem] overflow-y-auto">
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th className="text-left px-12"></th>
-                    <th className="text-left px-12">Track</th>
-                    <th className="text-left px-12">Artist</th>
-                    <th className="text-left px-12">Album</th>
-                    <th className="text-left px-12"></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {topTracks.items &&
-                    topTracks.items.map((track, index) => (
-                      <tr key={index}>
-                        <td>
-                          <div className="font-bold">#{index + 1}</div>
-                        </td>
-                        <td>
-                          <div className="flex items-center gap-3">
-                            <div className="avatar">
-                              <div className="w-12 h-12">
-                                <img
-                                  src={
-                                    track.album
-                                      ? track.album.images[0].url
-                                      : "https://upload.wikimedia.org/wikipedia/commons/b/b5/Windows_10_Default_Profile_Picture.svg"
-                                  }
-                                  alt="album_img"
-                                />
-                              </div>
-                            </div>
-                            <div className="font-bold">{track.name}</div>
-                          </div>
-                        </td>
-                        <th>
-                          <div className="font-bold">
-                            {track.artists[0].name}
-                          </div>
-                        </th>
-                        <th>
-                          <div className="font-bold">{track.album.name}</div>
-                        </th>
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+              </div>
             </div>
-            <p className="text-white pt-10 pb-10 py-2 font-bold text-4xl">
-              Top Artists
-            </p>
-            <div className="max-h-[20rem] overflow-y-auto">
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th className="text-left px-12"></th>
-                    <th className="text-left px-12">Artist</th>
-                    <th className="text-left px-12">Genre</th>
-                    <th className="text-left px-12">Followers</th>
-                    <th className="text-left px-12"></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {topArtists.items &&
-                    topArtists.items.map((artist, index) => (
-                      <tr key={index}>
-                        <td>
-                          <div className="font-bold">#{index + 1}</div>
-                        </td>
-                        <td>
-                          <div className="flex items-center gap-3">
-                            <div className="avatar">
-                              <div className="w-12 h-12">
-                                <img
-                                  src={
-                                    artist.images && artist.images.length > 0
-                                      ? artist.images[0].url
-                                      : "https://upload.wikimedia.org/wikipedia/commons/b/b5/Windows_10_Default_Profile_Picture.svg"
-                                  }
-                                  alt="album_img"
-                                />
-                              </div>
-                            </div>
-                            <div className="font-bold">{artist.name}</div>
-                          </div>
-                        </td>
-                        <th>
-                          <div className="font-bold">{artist.genres[0]}</div>
-                        </th>
-                        <th>
-                          <div className="font-bold">
-                            {artist.followers.total.toLocaleString()}
-                          </div>
-                        </th>
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+              </div>
+            </div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+              </div>
+            </div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+              </div>
+            </div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+              </div>
+            </div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <div className="card-body">
+                <h2 className="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+              </div>
             </div>
           </div>
         </div>
